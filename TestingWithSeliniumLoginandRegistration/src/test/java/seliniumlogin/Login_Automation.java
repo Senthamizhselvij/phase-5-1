cb52ae5862d1c1e36fb51c2fed3ffa548f1edcaf
senthamizhselvi <senthamizhselvi12112000@mail.com>
2022-10-02 21:37:12 +0530

    Automated webapp

---
+++ TestingWithSeliniumLoginandRegistration/src/test/java/seliniumlogin/Login_Automation.java
@@ -0,0 +1,30 @@
+package seliniumlogin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login_Automation {
+	 public static void main(String[] args)
+	    {
+	        System.setProperty(
+	            "webdriver.chrome.driver",
+	        		"C:\\Users\\Admins\\Downloads\\Selenium\\chromedriver_win32\\chromedriver.exe");
+	        WebDriver driver = new ChromeDriver();
+	        
+	        driver.get("https://profile.oracle.com/myprofile/account/create-account.jspx");
+	        
+	        driver.manage().window().maximize();
+	        
+	        driver.findElement(By.id("email"))
+	        .sendKeys("vishnu@gmail.com");
+	        
+	        
+	        driver.findElement(By.id("pass"))
+	        .sendKeys("vishnupriya@123");
+	        
+	        driver.findElement(By.id("loginbutton"))
+            .click();
+	        
+	    }
+}
